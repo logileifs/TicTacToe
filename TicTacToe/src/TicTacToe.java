b71302ed3ffc3d42214cfb53bbfb749ee40c353e
Logan Theragon <LoganTheragon@gmail.com>
2012-11-19 17:43:47 +0000

    Project created

---
+++ TicTacToe/src/TicTacToe.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: USER
+ * Date: 19.11.2012
+ * Time: 17:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class TicTacToe{
+
+    public static void main(String[] args) {
+        System.out.println("Tic Tac Toe");
+    }
+
+}
