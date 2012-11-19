a457339622c184bff0578f0f0f69b955cdd311b0
Heimir Már Helgason <heimirmar@gmail.com>
2012-11-19 20:01:01 +0000

    Need testing

---
+++ TicTacToe/src/TicTacToeTest.java
@@ -0,0 +1,31 @@
+/**
+* Created with IntelliJ IDEA.
+* User: USER
+* Date: 19.11.2012
+* Time: 17:58
+* To change this template use File | Settings | File Templates.
+*/
+
+
+
+public class TicTacToeTest extends junit.framework.TestCase {
+
+    /*public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    void addNameTest(){
+        TicTacToe t = new TicTacToe();
+        t.addNames();
+        assertEquals(t.player1, "player1");
+    }
+
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    } */
+}
+
+
+
+
