916c829a9ce3230bcdcbdd7716df29d32bb0ae44
obmagnusson <obmagnusson@gmail.com>
2012-11-25 22:54:13 +0000

    Ekkert nýtt sett inn bara verið að prufa build scriptu á server

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -23,7 +23,6 @@ public class TicTacToe{
         int count = 1;
         for(int i = 0 ; i < 3; i++){
             for(int j = 0; j < 3; j++){
-
                 board[i][j] = Integer.toString(count);
                 count++;
             }
